a9ac15f588850ad9efd00b0187eda5530b3ff320
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2023-02-06 12:14:20 -0300

    inspiration model

--- source/model/inspirationModel.js
+++ source/model/inspirationModel.js
@@ -5,8 +5,11 @@ const IspirationSchema = new mongoose.Schema(
         id: { type: String },
         'title': { type: String, required: true },
         'content': { type: String, required: true },
+        'content2': { type: String },
         'writer': { type: String, required: true },
         'image': { type: String, required: true },
+        'image2': { type: String },
+        'link': { type, String },
         'video': { type: String }
     }
 );
