857e13dfde569ac08d6b1bd2cb20a3853b6dd827
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2023-02-06 15:06:47 -0300

    nmber

--- source/model/inspirationModel.js
+++ source/model/inspirationModel.js
@@ -5,10 +5,10 @@ const IspirationSchema = new mongoose.Schema(
         id: { type: String },
         'title': { type: String, required: true },
         'tec': { type: mongoose.Schema.Types.ObjectId, ref: 'tec', required: true },
-        'content': { type: Array, required: true },
+        'content': { type: String, required: true },
         'dev': { type: String, required: true },
-        'image': { type: Array, required: true },
-        'difficult': { type: String, required: true },
+        'image': { type: String, required: true },
+        'difficult': { type: Number, required: true },
         'link': { type: String },
         'video': { type: String }
     }
